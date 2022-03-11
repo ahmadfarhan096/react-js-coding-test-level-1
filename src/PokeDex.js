24d45d26672bc71a20d7cd4cc6ded1db1d10171e
ahmadfarhan096 <farh96@gmail.com>
2022-03-11 10:58:19 +0800

    add second test

--- src/PokeDex.js
+++ src/PokeDex.js
@@ -3,27 +3,48 @@ import { useState, useEffect } from "react";
 import ReactLoading from "react-loading";
 import axios from "axios";
 import Modal from "react-modal";
+import "./PokeDex.css";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    background: "black",
+    color: "white",
+  },
+  overlay: { backgroundColor: "grey" },
+};
 
 function PokeDex() {
-  const [pokemons, setPokemons] = useState([]);
+  const [pokemons, setPokemons] = useState({});
+  const [filteredPokemons, setFilteredPokemons] = useState([]);
   const [pokemonDetail, setPokemonDetail] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const [url, setUrl] = useState(`https://pokeapi.co/api/v2/pokemon?limit=5&offset=0`);
 
-  const customStyles = {
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-      background: "black",
-      color: "white",
-    },
-    overlay: { backgroundColor: "grey" },
-  };
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const pokemons = await axios.get(url);
+      console.log(pokemons.data);
+      setPokemons(pokemons.data);
+      setIsLoading(false);
+    };
+    fetchPokemons();
+  }, [url]);
 
-  if (!isLoading && pokemons.length === 0) {
+  const searchPokemon = (event) => {
+    const text = event.target.value.toLowerCase();
+    const copy_pokemons = [...pokemons.results];
+    const filtered = copy_pokemons.results.filter(pokemon => pokemon.name.toLowerCase().indexOf(text) > - 1);
+    setFilteredPokemons(filtered);
+  }
+
+  if (!isLoading && pokemons?.results?.length === 0) {
     return (
       <div>
         <header className="App-header">
@@ -49,14 +70,30 @@ function PokeDex() {
     );
   }
 
+  const onOpenModal = (detail) => {
+    setOpenModal(true);
+    setPokemonDetail(detail)
+  }
+
+  const onCloseModal = () => {
+    setOpenModal(false);
+    setPokemonDetail(null)
+  }
+
   return (
     <div>
       <header className="App-header">
+        <input 
+          type="search" 
+          className="search-bar" 
+          placeholder="Search your favourite pokemon" 
+          onChange={searchPokemon} 
+        />
         {isLoading ? (
           <>
             <div className="App">
               <header className="App-header">
-                <b>Implement loader here</b>
+              <ReactLoading />
               </header>
             </div>
           </>
@@ -64,30 +101,32 @@ function PokeDex() {
           <>
             <h1>Welcome to pokedex !</h1>
             <b>Implement Pokedex list here</b>
+            <ul className="list">
+              {filteredPokemons.length ? filteredPokemons?.map((pokemon, idx) => (
+                <li className="item" key={idx} onClick={onOpenModal.bind(null, pokemon)}>
+                  <span>{pokemon.name}</span>
+                </li>
+              )): pokemons?.results?.map((pokemon, idx) => (
+                <li className="item" key={idx} onClick={onOpenModal.bind(null, pokemon)}>
+                  <span>{pokemon.name}</span>
+                </li>
+              ))}
+            </ul>
+            <div>
+              <button onClick={() => setUrl(pokemons.previous ?? url)} disabled={!pokemons.previous}>Previous</button>
+              <button onClick={() => setUrl(pokemons.next)} disabled={!pokemons.next}>Next</button>
+            </div>
           </>
         )}
       </header>
-      {pokemonDetail && (
+      {openModal && (
         <Modal
-          isOpen={pokemonDetail}
-          contentLabel={pokemonDetail?.name || ""}
-          onRequestClose={() => {
-            setPokemonDetail(null);
-          }}
+          isOpen={openModal}
+          onRequestClose={onCloseModal}
           style={customStyles}
+          ariaHideApp={false}
         >
-          <div>
-            Requirement:
-            <ul>
-              <li>show the sprites front_default as the pokemon image</li>
-              <li>
-                Show the stats details - only stat.name and base_stat is
-                required in tabular format
-              </li>
-              <li>Create a bar chart based on the stats above</li>
-              <li>Create a  buttton to download the information generated in this modal as pdf. (images and chart must be included)</li>
-            </ul>
-          </div>
+          <ModalContent url={pokemonDetail?.url} />
         </Modal>
       )}
     </div>
@@ -95,3 +134,42 @@ function PokeDex() {
 }
 
 export default PokeDex;
+
+function ModalContent({url}) {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    if (!url) return;
+    const get = async () => {
+      const res = await axios.get(url.toString());
+      console.log(res.data);
+      setData(res.data);
+    }
+    get();
+  }, [url]);
+  
+  if (!data) return <div>No Data Found</div>
+
+  return (
+    <div style={{ display: "grid", placeItems: "center" }}>
+      <img src={data.sprites?.front_default} />
+      <table>
+        <thead>
+          <tr>
+            <th align="left">Name</th>
+            <th align="left">Base Stat</th>
+          </tr>
+        </thead>
+      <tbody>
+      {data.stats?.map((stat, index) => {
+        return ( 
+          <tr key={index}>
+            <td>{stat.stat.name}</td>
+            <td>{stat.base_stat}</td>
+          </tr>
+        )
+      })}
+      </tbody>
+      </table>
+    </div>
+  )
+}
